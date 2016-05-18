7c699b445f769f05365144b56a792ed19b9468ac
Max Bo <max@maxbo.me>
2016-05-18 10:40:52 +1000

    :new: feat: Init

---
+++ main.c
@@ -0,0 +1,18 @@
+/*
+ * Tetris.c
+ *
+ * Created: 17/05/2016 11:49:58 PM
+ * Author : Max
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
