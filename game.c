9446723720fb65417658e7cdb5f20b69a4e86c9c
Maxwell Bo (s4392687) <s4392687@lichen.labs.eait.uq.edu.au>
2016-05-24 14:57:20 +1000

    :new: feat: Add scoring

--- game.c
+++ game.c
@@ -246,39 +246,56 @@ uint8_t fix_block_to_board_and_add_new_block(void) {
  */
 static void check_for_completed_rows(void) {
 
-
-
-	 	// TODO: What integer type can I use here?
-		// u_int8 or int?
+		uint8_t rows_cleared = 0;
+		uint8_t rows_cleared_successively = 0; // Number of rows that have been popped in a row
 		uint8_t tetris_count = 0;
-		uint8_t row_count = 0;
-
+		
 		for(uint8_t row=0; row < BOARD_ROWS; row++) {
 			if(board[row] == ((1 << BOARD_WIDTH) - 1)) {
-				// TODO: Might be a off by one error here. Check.
-				for(uint8_t i=row; i >= 1; i--) {
-
-					board[i] = board[i - 1];
+				// Found filled row
+				rows_cleared++;
+				rows_cleared_successively++;
+				
+				if (rows_cleared_successively >= 4) {
+					tetris_count++; 
 					
+					// Reset the counter
+					rows_cleared_successively = 0;
+					
+					// Remove those that "contributed" to the Tetris
+					rows_cleared -= 4;
+				}
+				
+				// Shift all rows down up until filled row
+				for(uint8_t i=row; i >= 1; i--) {
+					
+					board[i] = board[i - 1];
 					for(uint8_t j=0; j < MATRIX_NUM_ROWS; j++) {
 						board_display[i][j] = board_display[i - 1][j];
 					}
 				}
-
-				// Note that terminal.io has been imported
-
-				// clear_terminal();
-				// 
-				printf_P(PSTR("CSSE2010/7201 Tetris Project by Max Bo"));	
+				
+				// Empty the top row
 				board[0] = 0;
-
 				for(uint8_t i=0; i < MATRIX_NUM_ROWS; i++) {
 					board_display[0][i] = 0;
 				}
-
+				
 				update_rows_on_display(0, BOARD_ROWS);
+	
+			} else {
+				rows_cleared_successively = 0;
 			}
-		}
+			
+			// Cleanup
+			if (rows_cleared || tetris_count) {
+				add_to_score(rows_cleared * 100);
+				add_to_score(tetris_count * 800);
+				// clear_terminal();
+				printf_P(PSTR("Score: %d"), get_score());			
+			}
+
+		} // Loop close
 	/* Suggested approach is to iterate over all the rows (0 to
 	 * BOARD_ROWS -1) in the board and check if the row is all ones
 	 * i.e. matches ((1 << BOARD_WIDTH) - 1).
