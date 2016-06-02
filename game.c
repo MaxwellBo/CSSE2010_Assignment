8e14d9a65123d36d7667537d8f48b8a5d1ad34f4
Maxwell Bo (s4392687) <s4392687@lichen.labs.eait.uq.edu.au>
2016-06-02 12:40:32 +1000

    :new: feat: Terminal display

--- game.c
+++ game.c
@@ -88,6 +88,8 @@ void update_rows_on_display(uint8_t row_start, uint8_t num_rows) {
 	for(uint8_t row_num = row_start; row_num <= row_end; row_num++) {
 		ledmatrix_update_column(row_num, board_display[row_num]);
 	}
+	
+	print_game_view();
 }
 
 /*
@@ -376,7 +378,37 @@ void print_block_preview(void) {
 }
 
 void print_game_view(void) {
-	
+	for(uint8_t row=0; row < BOARD_ROWS; row++) {
+		for(uint8_t col=0; col < MATRIX_NUM_ROWS; col++) {
+			move_cursor(row, col);
+			
+			uint8_t pixel_color = board_display[row][col];
+			
+			uint8_t terminal_color = FG_RED;
+			
+			if (pixel_color == COLOUR_RED) {
+				terminal_color = FG_RED;
+			}
+			else if (pixel_color == COLOUR_GREEN) {
+				terminal_color = FG_GREEN;
+			}
+			else if (pixel_color == COLOUR_YELLOW) {
+				terminal_color = FG_YELLOW;
+			}
+			else if (pixel_color == COLOUR_ORANGE) {
+				terminal_color = FG_BLUE;
+			}
+			else if (pixel_color == COLOUR_LIGHT_ORANGE) {
+				terminal_color = FG_CYAN;
+			}
+						
+			set_display_attribute(terminal_color);
+			reverse_video();
+			printf_P(PSTR(" "));
+			normal_display_mode();
+					
+		}
+	}
 }
 
 /*
